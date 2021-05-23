ee798b96064e57bae2caebf325262aa3f62fc10e
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-23 21:09:47 +0530

    Remved bugs

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Text, View } from "react-native";
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
 
-const fetchFonts = () => {
-  Font.loadAsync({
-    "open-sans": require("./App/assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./App/assets/fonts/OpenSans-Bold.ttf"),
-  });
-};
-
-export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
-      />
-    );
-  }
+const App = () => {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Try editing me! 🎉</Text>
     </View>
   );
-}
+};
+
+export default App;
