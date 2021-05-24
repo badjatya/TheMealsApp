6295de0744e688c30aa390a6cfc6c2610ef1e908
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-24 22:48:54 +0530

    Using open sans font added and its configuration added in app

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 
+import * as Fonts from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Fonts.loadAsync({
+    "open-sans": require("./App/assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./App/assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello World!</Text>
+      <Text style={{ fontFamily: "open-sans-bold" }}>Hello World!</Text>
     </View>
   );
 };
