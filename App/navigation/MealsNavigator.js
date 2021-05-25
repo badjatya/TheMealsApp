7bdb99f09f85316507a933cec0b4963b7d8271e0
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-25 23:07:20 +0530

    meals upgrade

--- App/navigation/MealsNavigator.js
+++ App/navigation/MealsNavigator.js
@@ -1,16 +1,27 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import createStackNavigator from "react-navigation-stack";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealsScreen,
-  },
-  MealDetail: MealDetailScreen,
-});
+const MainStack = createStackNavigator();
 
-export default createAppContainer(MealsNavigator);
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
+      <MainStack.Screen name="Categories" component={CategoriesScreen} />
+      <MainStack.Screen name="Category Meals" component={CategoryMealsScreen} />
+      <MainStack.Screen name="Meal Details" component={MealDetailScreen} />
+    </MainStack.Navigator>
+  );
+};
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <MainStackScreen />
+    </NavigationContainer>
+  );
+};
