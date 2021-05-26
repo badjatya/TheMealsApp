3bc821471a7b6be29da3b868415b082f742aac1c
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-26 22:25:07 +0530

    going back to category screen using push

--- App/screens/CategoryMealsScreen.js
+++ App/screens/CategoryMealsScreen.js
@@ -12,9 +12,15 @@ const CategoryMealScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <View style={{ marginBottom: 20 }} />
       <Button
         title="Go to MealDetails!"
         onPress={() => props.navigation.push("Meal Details")}
+      />
+      <View style={{ marginBottom: 20 }} />
+      <Button
+        title="Go back to Categories"
+        onPress={() => props.navigation.pop()}
       />
     </View>
   );
