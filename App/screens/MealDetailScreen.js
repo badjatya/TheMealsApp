82f6437eb5fb370ebc35747cdcb35d4166ab96f5
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-26 22:26:35 +0530

    Going back to main screen

--- App/screens/MealDetailScreen.js
+++ App/screens/MealDetailScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const styles = StyleSheet.create({
   screen: {
@@ -9,10 +9,11 @@ const styles = StyleSheet.create({
   },
 });
 
-const MealDetailScreen = () => {
+const MealDetailScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Meal Detail Screen!</Text>
+      <Button title="Back" onPress={() => props.navigation.popToTop()} />
     </View>
   );
 };
