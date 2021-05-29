6659390d1a030a669be8505469bfb953c5c96651
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-29 22:22:43 +0530

    Styled CategoryGridTile component

--- App/components/CategoryGridTile.js
+++ App/components/CategoryGridTile.js
@@ -1,28 +1,55 @@
 import React from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableNativeFeedback,
+} from "react-native";
 
 const styles = StyleSheet.create({
   gridItem: {
     flex: 1,
     margin: 15,
     height: 150,
+    borderRadius: 10,
+    overflow: "hidden",
   },
   container: {
     flex: 1,
     borderRadius: 10,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    textAlign: "right",
   },
 });
 
 const CategoryGridTile = (props) => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === "android") {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
   return (
     <View style={styles.gridItem}>
-      <TouchableOpacity style={{ flex: 1 }} onPress={props.onSelect}>
+      <TouchableCmp style={{ flex: 1 }} onPress={props.onSelect}>
         <View
           style={{ ...styles.container, ...{ backgroundColor: props.color } }}
         >
-          <Text>{props.title}</Text>
+          <Text style={styles.title}>{props.title}</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableCmp>
     </View>
   );
 };
