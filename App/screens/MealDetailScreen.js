e17730d9d804bd9fdb6bd1e8f724f4dfb7f8bd26
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-30 14:34:53 +0530

    added dynamic header title

--- App/screens/MealDetailScreen.js
+++ App/screens/MealDetailScreen.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Button, Platform } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import Colors from "../constants/colors";
 
 const styles = StyleSheet.create({
   screen: {
@@ -10,9 +13,24 @@ const styles = StyleSheet.create({
 });
 
 const MealDetailScreen = (props) => {
+  const { mealId } = props.route.params;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  useEffect(() => {
+    props.navigation.setOptions({
+      title: selectedMeal.title,
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.primaryColor,
+    });
+  });
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button title="Back" onPress={() => props.navigation.popToTop()} />
     </View>
   );
