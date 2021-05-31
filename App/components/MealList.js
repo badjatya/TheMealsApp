93391e07f3de4ef3239c5773cd9c16f8d4140317
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-31 22:45:59 +0530

    MealList component

---
+++ App/components/MealList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+const styles= StyleSheet.create({
+    MealList: {
+        flex:1
+    }
+})
+
+const MealList = () => {
+    return ()
+}
+
+export default MealList;
