d2efe48c7fa4f19f9877baeacac5018c1a12cb49
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-02 00:37:54 +0530

    Hamburger added

--- App/navigation/MealsNavigator.js
+++ App/navigation/MealsNavigator.js
@@ -4,14 +4,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { Ionicons } from "@expo/vector-icons";
+import Icon from "react-native-vector-icons/Ionicons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
+import FiltersScreen from "../screens/FiltersScreen";
 
 import CustomHeaderIcon from "../components/CustomHeaderIcon";
 
@@ -19,7 +22,7 @@ import Colors from "../constants/colors";
 
 const MainStack = createStackNavigator();
 
-const MainStackScreen = () => {
+const MainStackScreen = ({ navigation }) => {
   return (
     <MainStack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
       <MainStack.Screen
@@ -33,6 +36,17 @@ const MainStackScreen = () => {
           },
           headerTintColor:
             Platform.OS === "android" ? "white" : Colors.primaryColor,
+          headerLeft: () => {
+            return (
+              <Icon
+                name="ios-menu"
+                size={30}
+                style={{ paddingLeft: 10, paddingTop: 5 }}
+                color="#fff"
+                onPress={() => navigation.openDrawer()}
+              />
+            );
+          },
         }}
       />
       <MainStack.Screen name="Category Meals" component={CategoryMealsScreen} />
@@ -44,7 +58,7 @@ const MainStackScreen = () => {
             <HeaderButtons HeaderButtonComponent={CustomHeaderIcon}>
               <Item
                 iconName="ios-star-outline"
-                title="Favourite"
+                title="Favorite"
                 onPress={() => alert("Hello")}
               />
             </HeaderButtons>
@@ -56,7 +70,7 @@ const MainStackScreen = () => {
 };
 
 const FavStackNavigator = createStackNavigator();
-const FavStackNavigatorScreen = () => {
+const FavStackNavigatorScreen = ({ navigation }) => {
   return (
     <FavStackNavigator.Navigator screenOptions={{ headerTitleAlign: "center" }}>
       <FavStackNavigator.Screen
@@ -70,6 +84,17 @@ const FavStackNavigatorScreen = () => {
           },
           headerTintColor:
             Platform.OS === "android" ? "white" : Colors.primaryColor,
+          headerLeft: () => {
+            return (
+              <Icon
+                name="ios-menu"
+                size={30}
+                style={{ paddingLeft: 10, paddingTop: 5 }}
+                color="#fff"
+                onPress={() => navigation.openDrawer()}
+              />
+            );
+          },
         }}
       />
     </FavStackNavigator.Navigator>
@@ -81,44 +106,94 @@ const FavoriteBottomTab =
     ? createMaterialBottomTabNavigator()
     : createBottomTabNavigator();
 
+const FavoriteBottomTabScreen = () => {
+  return (
+    <FavoriteBottomTab.Navigator
+      activeColor="#fff"
+      shifting="true"
+      barStyle={{ backgroundColor: Colors.primaryColor }}
+      tabBarOptions={{
+        activeTintColor: Colors.accentColor,
+      }}
+    >
+      <FavoriteBottomTab.Screen
+        name="Home"
+        component={MainStackScreen}
+        options={{
+          tabBarIcon: (tabInfo) => {
+            return (
+              <Ionicons name="ios-restaurant" size={25} color={tabInfo.color} />
+            );
+          },
+        }}
+      />
+      <FavoriteBottomTab.Screen
+        name="Favorites"
+        component={FavStackNavigatorScreen}
+        options={{
+          tabBarIcon: (tabInfo) => {
+            return <Ionicons name="ios-star" size={25} color={tabInfo.color} />;
+          },
+        }}
+      />
+    </FavoriteBottomTab.Navigator>
+  );
+};
+
+// TODO: Hamburger Menu
+
+const FilterStackNavigator = createStackNavigator();
+const FilterStackNavigatorScreen = ({ navigation }) => {
+  return (
+    <FilterStackNavigator.Navigator
+      screenOptions={{ headerTitleAlign: "center" }}
+    >
+      <FilterStackNavigator.Screen
+        name="Filters"
+        component={FiltersScreen}
+        options={{
+          title: "Filters",
+          headerStyle: {
+            backgroundColor:
+              Platform.OS === "android" ? Colors.primaryColor : "",
+          },
+          headerTintColor:
+            Platform.OS === "android" ? "white" : Colors.primaryColor,
+          headerLeft: () => {
+            return (
+              <Icon
+                name="ios-menu"
+                size={30}
+                style={{ paddingLeft: 10, paddingTop: 5 }}
+                color="#fff"
+                onPress={() => navigation.openDrawer()}
+              />
+            );
+          },
+        }}
+      />
+    </FilterStackNavigator.Navigator>
+  );
+};
+
+const HamburgerDrawer = createDrawerNavigator();
+
+const HamburgerDrawerScreen = () => {
+  return (
+    <HamburgerDrawer.Navigator>
+      <HamburgerDrawer.Screen name="Home" component={FavoriteBottomTabScreen} />
+      <HamburgerDrawer.Screen
+        name="Filter"
+        component={FilterStackNavigatorScreen}
+      />
+    </HamburgerDrawer.Navigator>
+  );
+};
+
 export default () => {
   return (
     <NavigationContainer>
-      <FavoriteBottomTab.Navigator
-        activeColor="#fff"
-        shifting="true"
-        barStyle={{ backgroundColor: Colors.primaryColor }}
-        tabBarOptions={{
-          activeTintColor: Colors.accentColor,
-        }}
-      >
-        <FavoriteBottomTab.Screen
-          name="Home"
-          component={MainStackScreen}
-          options={{
-            tabBarIcon: (tabInfo) => {
-              return (
-                <Ionicons
-                  name="ios-restaurant"
-                  size={25}
-                  color={tabInfo.color}
-                />
-              );
-            },
-          }}
-        />
-        <FavoriteBottomTab.Screen
-          name="Favorites"
-          component={FavStackNavigatorScreen}
-          options={{
-            tabBarIcon: (tabInfo) => {
-              return (
-                <Ionicons name="ios-star" size={25} color={tabInfo.color} />
-              );
-            },
-          }}
-        />
-      </FavoriteBottomTab.Navigator>
+      <HamburgerDrawerScreen />
     </NavigationContainer>
   );
 };
