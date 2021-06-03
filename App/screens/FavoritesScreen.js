ddd8b07c4ebba110e45e93b7bdd62d307c8eb374
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-03 22:48:16 +0530

    Added react store in FavoriteScreen

--- App/screens/FavoritesScreen.js
+++ App/screens/FavoritesScreen.js
@@ -1,13 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import MealList from "../components/MealList";
 
-import { MEALS } from "../data/dummy-data";
-
 const FavoritesScreen = (props) => {
-  const favMeals = MEALS.filter(
-    (meals) => meals.id === "m1" || meals.id === "m2"
-  );
+  const favMeals = useSelector((state) => state.meals.favoriteMeals);
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
